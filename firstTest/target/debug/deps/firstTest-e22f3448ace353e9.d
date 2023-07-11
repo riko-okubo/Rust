2f40220aa9a4df2a183e45f1ab5d46c0a71dac4f
riko-okubo <12riko14@gmail.com>
2023-07-11 17:08:12 +0900

    mainで再commit(?)

---
+++ firstTest/target/debug/deps/firstTest-e22f3448ace353e9.d
@@ -0,0 +1,5 @@
+/Users/riko_okubo/Documents/Develop/Rust/firstTest/target/debug/deps/firstTest-e22f3448ace353e9.rmeta: src/main.rs
+
+/Users/riko_okubo/Documents/Develop/Rust/firstTest/target/debug/deps/firstTest-e22f3448ace353e9.d: src/main.rs
+
+src/main.rs:
