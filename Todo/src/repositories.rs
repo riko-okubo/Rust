4bc160f9ab854e857a885e45fcfe972ed6697676
riko-okubo <12riko14@gmail.com>
2023-07-17 15:39:36 +0900

    5つの関数を追記

--- Todo/src/repositories.rs
+++ Todo/src/repositories.rs
@@ -1,10 +1,12 @@
 use std::{
     collections::HashMap,
-    sync::{Arc, RwLock},
+    sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard},
 };
 
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+
+use anyhow::{Context, Ok};
 
 #[derive(Debug, Error)] //リポジトリで発生し得るエラーを定義
 enum RepositoryError {
@@ -63,6 +65,55 @@ impl TodoRepositoryForMemory {
             store: Arc::default(),
         }
     }
+
+    fn write_store_ref(&self) -> RwLockWriteGuard<TodoDatas> {
+        self.store.write().unwrap()
+    }
+
+    fn read_store_ref(&self) -> RwLockReadGuard<TodoDatas> {
+        self.store.read().unwrap()
+    }
+
+    fn create(&self, payload: CreateTodo) -> Todo {
+        let mut store = self.write_store_ref();
+        let id = (store.len() + 1) as i32;  //保存済みの長さ＋１、asは明示的な型の変換
+        let todo = Todo::new(id, payload.text.clone());
+        store.insert(id, todo.clone()); // insertで追加storeへ
+        todo
+    }
+
+    fn find(&self, id: i32) -> Option<Todo> {
+        let store = self.read_store_ref();
+        store.get(&id).map(|todo| todo.clone())
+    }
+
+    fn all(&self) -> Vec<Todo> {
+        let store = self.read_store_ref();
+        Vec::from_iter(store.values().map(|todo| todo.clone()))
+    }
+
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
+        let mut store = self.write_store_ref();
+        let todo = store
+            .get(&id)
+            .context(RepositoryError::NotFound(id))?;
+        let text = payload.text.unwrap_or(todo.text.clone());
+        let completed = payload.completed.unwrap_or(todo.completed);
+        let todo = Todo {
+            id,
+            text,
+            completed,
+        };
+        store.insert(id, todo.clone());
+        Ok(todo)
+    }
+
+    fn deleted(&self, id: i32) -> anyhow::Result<()> {
+        let mut store = self.write_store_ref();
+        store.remove(&id).ok_or(RepositoryError::NotFound(id))?;
+        Ok(())
+    }
+
 }
 
 impl TodoRepository for TodoRepositoryForMemory {
